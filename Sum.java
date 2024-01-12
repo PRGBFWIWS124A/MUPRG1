027e7000b736400916cb2543677709d02b39bc53
Marcel-FH <marcel.ullrich@edu.fhdw.de>
2024-01-12 12:34:30 +0100

    Sum

---
+++ Sum.java
@@ -0,0 +1,11 @@
+package MUPRG1;
+
+public class Sum {
+    public static void main(String[] args) {
+        int result = 0;
+        for(int i = 0; i < args.length; i++){
+            result += Integer.valueOf(args[i]);
+        }
+        System.out.println("" + result);
+    }
+}

1785c54cd21f224849ab329f190dbf2b2e0bacf7
Marcel-FH <marcel.ullrich@edu.fhdw.de>
2024-01-12 12:59:16 +0100

    sum

--- Sum.java
+++ Sum.java
@@ -3,9 +3,18 @@ package MUPRG1;
 public class Sum {
     public static void main(String[] args) {
         int result = 0;
+
         for(int i = 0; i < args.length; i++){
-            result += Integer.valueOf(args[i]);
+            int zahl = 0;
+            try {
+                zahl = Integer.valueOf(args[i]);
+            }catch(Exception e){
+                System.out.println("\nDie Variable war keine Zahl!  =>  " + args[i] + "\n");
+            }
+
+            result += zahl;
         }
+
         System.out.println("" + result);
     }
 }

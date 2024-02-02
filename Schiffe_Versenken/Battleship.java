dd05cfbafb38a7318533632cf15fea5c58efa5cd
Marcel-FH <marcel.ullrich@edu.fhdw.de>
2024-02-02 09:57:32 +0100

    debug

--- Schiffe_Versenken/Battleship.java
+++ Schiffe_Versenken/Battleship.java
@@ -11,7 +11,7 @@ public class Battleship {
     }
 
     static int distance(final Coordinate start, final Coordinate end) {
-        return Math.abs(((start.column() - end.column()) + (start.row() - end.row())));
+        return Math.abs(((start.column() - end.column()))) + Math.abs((start.row() - end.row()));
     }
 
     static Coordinate getRandomCoordinate() {
